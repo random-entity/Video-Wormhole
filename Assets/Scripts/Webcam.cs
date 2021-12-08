936099cc306d76fd68c8b616ea5d0aa958820918
random-entity <73927400+random-entity@users.noreply.github.com>
2021-12-08 16:25:54 +0900

    devices check

--- Assets/Scripts/Webcam.cs
+++ Assets/Scripts/Webcam.cs
@@ -10,6 +10,11 @@ public class Webcam : MonoBehaviour
     private void Start()
     {
         devices = WebCamTexture.devices;
+
+        for(int i = 0; i < devices.Length; i++) {
+            Debug.Log(i + " : " + devices[i].name);
+        }
+
         webCamTexture = new WebCamTexture();
         webCamTexture.deviceName = devices[deviceIndex].name;
         webCamTexture.Play();
